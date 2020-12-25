f5d827a16df271d21b3dd75f6ba6e4b48af4ef5a
Azizul Tareq <azizul.tareq@northsouth.edu>
2020-12-25 20:08:10 +0600

    random users data is fetched

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const App = () => {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchUsers = async() => {
+            setLoading(true);
+            const res = await axios.get('https://randomuser.me/api/?page=1&results=50&nat=us');
+            setLoading(false);
+            setUsers(res.data.results);
+        };
+        fetchUsers();
+    }, [])
+
+
     return (
         <div>
             Pagination
+            {loading ? <p>Loading..</p> : <>
+                Data
+            </>
+            }
         </div>
     )
 }
